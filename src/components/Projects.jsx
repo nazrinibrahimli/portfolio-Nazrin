6dc7c966d9a80e795a842f78e514186cfa70d1bc
Nazrin İbrahimli <nibrahimli4676@ada.edu.az>
2023-03-13 14:40:24 +0400

    added new porject

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,6 +10,10 @@ import tasktracker from '../images/task-tracker.png'
 import caspiansoft from '../images/caspiansoft-3.png'
 import flutter from '../images/flutter-triple.png'
 import agency from '../images/agency.png'
+import dejavu from '../images/dejavu.png'
+
+
+
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -29,10 +33,13 @@ const Projects = () => {
         <Col><TwiceCard link="https://e-commerce-bookstore-five.vercel.app/" title="E-commerse Bookshop" image={ecommerce}  info="E-commerce website is written in React and Tailwind CSS . It is a fully responsive website." />
         </Col>
         </Row>
-      <Row>
+        <Row>
+        <Col><TwiceCard link="https://dejavu-medical-clinic.vercel.app/" title="Dejavu Clinic" image={dejavu}  info="Website of Dejavu Clinic that is written in React, Next JS, Mui and Tailwind CSS . It is a fully responsive website." />
+        </Col>
         <Col><TwiceCard link="https://github.com/nazrinibrahimli/Booking" title="Flutter Booking App" image={flutter}  info="Ticket Booking App is written with advanced Flutter concepts." />
         </Col>
-    
+        </Row>
+      <Row>
         <Col><TwiceCard  link="https://github.com/nazrinibrahimli/Restaurant-website" title="Dine Out Restaurant" image={restaurant} info="Restaurant website is a fully responsive and written with Vanilla Javascript, HTML and CSS."/>
         </Col>
         <Col><TwiceCard link="https://github.com/nargayeva/caspiansoft" title="Caspian Soft Company's Website" image={caspiansoft}  info="Caspian Soft company's website. Written with Vanilla Javascript, SASS, and HTML. It is responsive website." />
